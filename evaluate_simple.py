fd71eb8ac924cac7d247876eceeb728f7c4ec7cd
ammmy <sequrity_bb_61@yahoo.co.jp>
2017-04-08 17:29:46 +0900

    Update evaluate_simple.py
    add if __name__ == "__main__":

--- evaluate_simple.py
+++ evaluate_simple.py
@@ -1,5 +1,6 @@
 from model_simple import model
 from config_simple import *
 import logreg
-logreg.train(model, dim_embed=dim_embed, class_num=class_num, learning_rate=learning_rate)
+if __name__ == "__main__":
+    logreg.train(model, dim_embed=dim_embed, class_num=class_num, learning_rate=learning_rate)
 
